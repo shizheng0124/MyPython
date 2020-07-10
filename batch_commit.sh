0f4e0136f793f7c299c5ec6de9763e610f4d3920
shizheng0124 <shizheng0124@gmail.com>
2020-07-11 00:56:02 +0800

    push files

--- batch_commit.sh
+++ batch_commit.sh
@@ -3,5 +3,6 @@
 cd /home/pi/MyPython 
 git add .
 git commit -m "push files"
+# git remote add origin https://github.com/shizheng0124/MyPython.git
 git push origin master
 # 保存
--- sample.c
+++
@@ -1,6 +0,0 @@
-#include<studio.h>
-int main()
-{
-	printf("hello world!");
-	return 0;
-}
